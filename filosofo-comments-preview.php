3db048584d909606dc51d5a57c33793d29d44461
matzko <austin@pressedcode.com>
2005-09-03 18:50:18 +0000

    test revision

--- filosofo-comments-preview.php
+++ filosofo-comments-preview.php
@@ -8,7 +8,7 @@ Author: Austin Matzko
 Author URI: http://www.ilfilosofo.com/blog/
 */
 
-/*  Copyright 2005  Austin Matzko  (email : if.website@gmail.com)
+/*  Copyright 2005  Austin Matzko  (email : if.website at gmail.com)
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
